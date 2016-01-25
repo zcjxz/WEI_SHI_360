ef37ab9a28a6ebb0d6f66e436f344697b3aea33a
zcjxz <1067986407@qq.com>
2016-01-25 20:21:23 +0800

    再试一下

---
+++ app/src/main/java/com/zcj/wei_shi_360/activity/setup3.java
@@ -0,0 +1,16 @@
+package com.zcj.wei_shi_360.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zcj.wei_shi_360.R;
+
+public class setup3 extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup3);
+    }
+}
