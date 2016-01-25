7bf8f0db513e0cf95e2737eb43cb44d698285c5e
zcjxz <1067986407@qq.com>
2016-01-25 17:41:24 +0800

    006

--- app/src/main/java/com/zcj/wei_shi_360/activity/Setup2Activity.java
+++ app/src/main/java/com/zcj/wei_shi_360/activity/Setup2Activity.java
@@ -2,18 +2,14 @@ package com.zcj.wei_shi_360.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
 import com.zcj.wei_shi_360.R;
 
-public class Setup1 extends AppCompatActivity {
+public class Setup2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setup1);
-    }
-    public void next(View view){
-        
+        setContentView(R.layout.activity_setup2);
     }
 }
